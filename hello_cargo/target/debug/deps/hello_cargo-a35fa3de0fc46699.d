b998394b50ca4b6d7c3f91abb8b9f240c206de43
Jared Clayborn <jared.the.engineer1@gmail.com>
2025-12-13 09:52:07 -0700

    starting cargo

---
+++ hello_cargo/target/debug/deps/hello_cargo-a35fa3de0fc46699.d
@@ -0,0 +1,5 @@
+/home/jaredclayborn/dev/hello_world_rust/hello_cargo/target/debug/deps/hello_cargo-a35fa3de0fc46699.d: src/main.rs
+
+/home/jaredclayborn/dev/hello_world_rust/hello_cargo/target/debug/deps/hello_cargo-a35fa3de0fc46699: src/main.rs
+
+src/main.rs:
